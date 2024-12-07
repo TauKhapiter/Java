7330f6fa2344f59f60d81f1452b5babe29349163
TauKhapiter <sunnyuiet2017@gmail.com>
2024-12-08 01:53:42 +0530

    Inserting Lines of code.

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+// write our first program in java to push on github
+
+public class FirstJava{
+    public static void main(String[] args){
+            System.out.println("Hello World !!");
+    }
+}
